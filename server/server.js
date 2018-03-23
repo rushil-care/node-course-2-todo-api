9450a9ca8d60a24e7b907cd552fa75353345c600
root <root@rushil>
2018-03-23 10:58:48 +0530

    ADD LOgout del

--- server/server.js
+++ server/server.js
@@ -51,24 +51,36 @@ app.post('/users',(req,res)=>{
 
 });
 
-//POST /USERS/login {email,password}
-app.post('/users/login',(req,res) => {
-    var body = _.pick(req.body,['email','password']);
-    User.findByCredentials(body.email , body.password).then((user)=>{
-          user.generateAuthToken().then((token)=>{
-                res.header('x-auth',token).send(user);
-                
-          });
-    }).catch((e)=> {
-        res.status(400).send();
-    });
-});
+
 // PRIVATE ROUTES
 
 
 app.get('/users/me',authenticate,(req,res)=>{
 
 res.send(req.user);
+});
+
+//POST /USERS/login {email,password}
+app.post('/users/login',(req,res) => {
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email , body.password).then((user)=>{
+          user.generateAuthToken().then((token)=>{
+                res.header('x-auth',token).send(user);
+
+          });
+    }).catch((e)=> {
+        res.status(400).send();
+    });
+});
+
+app.delete('/users/me/token',authenticate, (req, res)=> {
+
+    req.user.removeToken(req.token).then(()=> {
+      res.status(200).send();
+    },()=>{
+      res.status(400).send();
+    });
+
 });
 
 app.listen(3000,()=>{
